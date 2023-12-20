bd7fa53fd31a514049da31fa750e2604adf38273
2tinz <eridiongj@gmail.com>
2023-12-20 19:58:15 +0100

    Add files via upload

---
+++ grantMinterRole.ts
@@ -0,0 +1,28 @@
+import { getDefaultProvider, Wallet } from 'ethers'; // ethers v5
+import { Provider, TransactionResponse } from '@ethersproject/providers'; // ethers v5
+import { ERC721Client } from '@imtbl/contracts';
+
+const CONTRACT_ADDRESS = '0x57c210e573b694bd1d7ebcd5d05a7fac9d8a1bc6';
+const PRIVATE_KEY = '25cf506ce501708263c160cff5c98828c4afe275a9d65e6f03d245b252aa3230';
+const provider = getDefaultProvider('https://rpc.testnet.immutable.com');
+
+const grantMinterRole = async (
+  provider: Provider
+): Promise<TransactionResponse> => {
+  // Bound contract instance
+  const contract = new ERC721Client(CONTRACT_ADDRESS);
+  // The wallet of the intended signer of the mint request
+  const wallet = new Wallet(PRIVATE_KEY, provider);
+
+  // Give the wallet minter role access
+  const populatedTransaction = await contract.populateGrantMinterRole(
+    wallet.address, {
+  maxPriorityFeePerGas: 100e9,
+  maxFeePerGas: 150e9
+}
+  );
+  const result = await wallet.sendTransaction(populatedTransaction);
+  return result;
+};
+
+grantMinterRole(provider);
